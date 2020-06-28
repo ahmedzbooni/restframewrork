2d05edab8d65babe0a980cf971076e0c2eae463e
ahmed <ahmed@zbooni.com>
2020-06-28 14:49:01 +0400

    first commit

---
+++ api_basic/urls.py
@@ -0,0 +1,18 @@
+from .views import article_list, ArticleViewSet, article_detail, ArticleAPIView,ArticleDetail,GenericAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('article', ArticleViewSet,basename='article')
+
+urlpatterns = [
+	
+	path('viewset/', include(router.urls)),
+	path('viewset/<int:pk>/', include(router.urls)),
+    #path('article/', article_list),
+    #path('detail/<int:pk>/', article_detail),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetail.as_view()),
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
+    
+]
